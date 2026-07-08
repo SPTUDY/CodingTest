d6fd4bf4a28b4b56a02ce102b1d78fa74a5262e8
hyerirn <hyerim1148@naver.com>
2026-07-09 02:08:22 +0900

    coding test day2

---
+++ hyerim/260708/P018.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class P018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+
+        int sum = 0;
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            answer += sum;
+        }
+        System.out.println(answer);
+    }
+}
